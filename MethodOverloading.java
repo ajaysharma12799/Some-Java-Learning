949edadbc303511737edbe588bd9b35d7c502e78
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-12-25 10:41:19 +0530

    method Overloading Demo

---
+++ MethodOverloading.java
@@ -0,0 +1,15 @@
+public class MethodOverloading {
+	
+	// public static void sayHi();
+
+	public static void main(String[] args) {
+
+		sayHi(); // invoking Method
+		
+	}
+
+	public static void sayHi() {
+		System.out.println("Hi From Java");
+	}
+
+}
